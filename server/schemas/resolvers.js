006bd6e7c77312a23570351f3f4a7cf52b90f242
Tabitha Buder <tabibuder@gmail.com>
2024-04-12 16:34:23 -0700

    added schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,62 @@
+const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+                return userData;
+            }
+            throw AuthenticationError;
+        },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+            const correctPW = await user.isCorrectPassword(password);
+
+            if (!correctPW) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args.input } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                console.log(context.user);
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
+                console.log(updatedUser);
+                return updatedUser;
+            }
+            throw AuthenticationError;
+        },
+    },
+};
+
+module. exports = resolvers;
